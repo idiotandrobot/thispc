13fddab4fac0d520d7cf87dfc44b419049216d71
Michael Phillips <github@idiotandrobot.com>
2014-05-03 10:06:09 +0100

    VS never seems to add new files, what's that about?

---
+++ source/This PC/FolderViewModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThisPC
+{
+    public class FolderViewModel : INotifyPropertyChanged
+    {
+        FolderKey FolderKey, FolderKey32;
+
+        public FolderViewModel(string name, string key, RootKey nameSpace, RootKey nameSpace32)
+        {
+            Name = name;
+            FolderKey = new FolderKey(key, nameSpace);
+            FolderKey32 = new FolderKey(key, nameSpace32);
+        }
+
+        private string name;
+        public string Name 
+        {
+            get { return name; } 
+            private set
+            {
+                name = value;
+                NotifyPropertyChanged();
+            }
+        }
+        
+        public bool IsVisible
+        {
+            get 
+            {
+                return FolderKey.IsVisible
+                    || FolderKey32.IsVisible;
+            }
+            set
+            {
+                FolderKey.IsVisible = value;
+                FolderKey32.IsVisible = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void NotifyPropertyChanged([CallerMemberName] String propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (null != handler)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
---
+++ source/This PC/RootKey.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThisPC
+{
+    public class RootKey
+    {
+        private RegistryKey ReadOnlyKey;
+
+        public string Path { get; private set; }
+
+        public RootKey(string path)
+        {
+            Path = path;
+            ReadOnlyKey = GetReadOnlyKey();
+        }
+
+        public bool HasSubKey(string key)
+        {
+            return ReadOnlyKey.GetSubKeyNames().Contains(key);
+        }
+
+        public void AddSubKey(string key)
+        {
+            try
+            {
+                using (var writeableKey = GetWriteableKey())
+                {
+                    writeableKey.CreateSubKey(key);
+                }
+            }
+            catch (UnauthorizedAccessException) { }
+        }
+
+        public void RemoveSubKey(string key)
+        {
+            try
+            {
+                using (var writeableKey = GetWriteableKey())
+                {
+                    writeableKey.DeleteSubKey(key);
+                }
+            }
+            catch (UnauthorizedAccessException) { }
+        }
+
+        private RegistryKey GetReadOnlyKey()
+        {
+            return GetKey();
+        }
+
+        private RegistryKey GetWriteableKey()
+        {
+            return GetKey(true);
+        }
+
+        private RegistryKey GetKey(bool writable = false)
+        {
+            try
+            {
+                return Registry.LocalMachine.OpenSubKey(Path, writable);
+            }
+            catch (SecurityException)
+            {
+                return GetKey();
+            }
+        }
+    }
+}
