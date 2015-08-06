1b0ba5f768902a2c23e792cf5ca13d2c88d91064
Michael Phillips <github@idiotandrobot.com>
2015-08-06 11:06:06 +0100

    Use Mono.Options to manage command line Remove space from exe name

--- source/This PC/App.xaml.cs
+++ source/This PC/App.xaml.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Mono.Options;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -13,6 +15,9 @@ namespace ThisPC
     /// </summary>
     public partial class App : Application
     {
+        [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
+        private static extern bool AttachConsole(int processId);
+
         public static MainViewModel ViewModel = new MainViewModel();
 
         private void Application_Startup(object sender, StartupEventArgs e)
@@ -24,20 +29,61 @@ namespace ThisPC
                 return;
             }
 
-            if (HasArgument(e, "/HideAll"))
+            AttachConsole(-1);
+
+            bool showHelp = false;
+            bool runShowAll = false;
+            bool runHideAll = false;
+            var p = new OptionSet() {
+                { "showall", "Show all User folders in This PC",
+                  v => runShowAll = true },
+                { "hideall",  "Hide all User folders in This PC",
+                  v => runHideAll = true },
+                { "?|h|help",  "show this message and exit",
+                  v => showHelp = v != null },
+            };
+
+            List<string> extra;
+            try
+            {
+                extra = p.Parse(e.Args);
+            }
+            catch (OptionException ex)
+            {
+                Console.Write("thispc: ");
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Try `thispc --help' for more information.");
+                Application.Current.Shutdown();
+                return;
+            }
+
+            if (showHelp)
+            {
+                ShowHelp(p);
+            }
+            else if (runHideAll)
             {
                 HideAll();
             }
-            else if (HasArgument(e, "/ShowAll"))
+            else if (runShowAll)
             {
                 ShowAll();
             }
+
             Application.Current.Shutdown();
         }
 
-        private static bool HasArgument(StartupEventArgs e, string argument)
+        static void ShowHelp(OptionSet p)
         {
-            return e.Args.Contains(argument, StringComparer.OrdinalIgnoreCase);
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("Usage: thispc [OPTIONS]");
+            Console.WriteLine("");
+            Console.WriteLine("Alter the list of User folder links displayed in Windows Explorer This PC.");
+            Console.WriteLine("Running with no options will launch the UI.");
+            Console.WriteLine();
+            Console.WriteLine("Options:");
+            p.WriteOptionDescriptions(Console.Out);
         }
 
         private static void ShowAll()
