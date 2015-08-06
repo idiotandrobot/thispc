28d29315b5d6496487e5f4bd9da4f31faef5c4d7
Michael Phillips <github@idiotandrobot.com>
2015-08-06 09:22:43 +0100

    Add /ShowAll and /HideAll command line arguments

--- source/This PC/App.xaml.cs
+++ source/This PC/App.xaml.cs
@@ -14,5 +14,46 @@ namespace ThisPC
     public partial class App : Application
     {
         public static MainViewModel ViewModel = new MainViewModel();
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            if (e.Args.Length == 0)
+            {
+                var main = new MainWindow();
+                main.Show();
+                return;
+            }
+
+            if (HasArgument(e, "/HideAll"))
+            {
+                HideAll();
+            }
+            else if (HasArgument(e, "/ShowAll"))
+            {
+                ShowAll();
+            }
+            Application.Current.Shutdown();
+        }
+
+        private static bool HasArgument(StartupEventArgs e, string argument)
+        {
+            return e.Args.Contains(argument, StringComparer.OrdinalIgnoreCase);
+        }
+
+        private static void ShowAll()
+        {
+            foreach (var folder in ViewModel.Folders)
+            {
+                folder.IsVisible = true;
+            }
+        }
+
+        private static void HideAll()
+        {
+            foreach (var folder in ViewModel.Folders)
+            {
+                folder.IsVisible = false;
+            }
+        }
     }
 }
