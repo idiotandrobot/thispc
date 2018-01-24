ae9b4c107b33f518f099ffbc033f0d771cbe6dc1
Michael Phillips <github@idiotandrobot.com>
2018-01-24 22:28:25 +0000

    Update project

--- source/This PC/Properties/AssemblyInfo.cs
+++ source/This PC/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("This PC")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
